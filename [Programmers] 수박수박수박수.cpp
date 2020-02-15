91601e877b70f8f9755c382983b32425fbf15afd
seoyeonkK <syeon0204@naver.com>
2020-02-16 01:55:06 +0900

    Create [Programmers] 수박수박수박수.cpp

---
+++ [Programmers] 수박수박수박수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    
+    for (int i = 1; i <= n; i++) {
+        if (i%2) 
+            answer += "수";
+        else 
+            answer += "박";
+    }
+    
+    return answer;
+}
