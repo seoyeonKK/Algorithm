3a18eeaba1bd9c60a79964f612fb456220b60bf8
seoyeonKK <syeon0204@naver.com>
2020-03-03 17:28:56 +0900

    Update [BOJ] 14890 경사로.cpp
    [시뮬레이션]
    소요시간 약 2시간 반.
    너무 오래걸렸고 정말 지저분하게 풀었다.
    예제를 자세히 보지 않아 행 - 열 계산에서 각각 visited의 초기화 필요성을 몰랐고,
    k의 범위 설정을 나름 머리 써서 하다가 실수한 부분이 있었다.
    덕분에 디버깅 연습을 많이 할 수 있었던 문제였다 :)

--- [BOJ] 14890 경사로.cpp
+++ [BOJ] 14890 경사로.cpp
@@ -1,103 +1,97 @@
 #include <iostream>
+#include <cstring>
 #define MAX 100
 using namespace std;
 
 int N, L;
 int map[MAX][MAX];
-int visit[MAX][MAX];
+int visited[MAX][MAX];
 int ans = 0;
+bool flag = 0;
 
-void construct() {
-    bool flag = 0;
+void row() {
 
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N - 1; j++) {
+
             flag = 0;
-            if (!visit[i][j]) {
-                if (1 == map[i][j] - map[i][j+1]) {
-                    for (int k = 1; k < L; k++) {
-                        if (visit[i][j + k + 1] || (j + k + 1>= N) || (map[i][j + k + 1] != map[i][j + 1])) {
-                            flag = 1;
-                            break;
-                        }
 
-                        visit[i][j + 1] = 1;
-                        visit[i][j + k + 1] = 1;
+            if (1 == map[i][j] - map[i][j + 1]) {
+                for (int k = 0; k < L; k++) {
+                    if (visited[i][j + k + 1] || (j + k + 1 >= N) || (map[i][j + k + 1] != map[i][j + 1])) {
+                        flag = 1;
+                        break;
                     }
-                }
-                else if (-1 == map[i][j] - map[i][j+1]) {
-                    for (int k = 1; k < L; k++) {
-                        if (visit[i][j - k] || (j - k < 0) || (map[i][j - k] != map[i][j])) {
-                            flag = 1;
-                            break;
-                        }
 
-                        visit[i][j] = 1;
-                        visit[i][j - k] = 1;
-                    }
-                }
-
-                if (flag == 1) {
-                    memset(visit[i], 0, sizeof(int)*N);
-                    break;
+                    visited[i][j + 1] = 1;
+                    visited[i][j + k + 1] = 1;
                 }
             }
-            else {
-                memset(visit[i], 0, sizeof(int)*N);
+            else if (-1 == map[i][j] - map[i][j + 1]) {
+                for (int k = 0; k < L; k++) {
+                    if (visited[i][j - k] || (j - k < 0) || (map[i][j - k] != map[i][j])) {
+                        flag = 1;
+                        break;
+                    }
+
+                    visited[i][j] = 1;
+                    visited[i][j - k] = 1;
+                }
+            }
+            else if (map[i][j] != map[i][j + 1]) {
+                flag = 1;
                 break;
             }
-        }
-        if (flag == 0) {
-            ans++;
-        }
-    }
 
-    cout << "========" << endl;
+            if (flag) break;
+        }
+
+        memset(visited[i], 0, sizeof(int) * N);
+
+        if (!flag) ans++;
+    }
+}
+
+void column() {
 
     for (int j = 0; j < N; j++) {
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N-1; i++) {
+
             flag = 0;
 
-            if (!visit[i][j]) {
-                if (1 == map[i][j] - map[i + 1][j]) {
-                    for (int k = 1; k < L; k++) {
-                        if (visit[i + k + 1][j] || (i + k + 1 >= N) || (map[i + k + 1][j] != map[i + 1][j])) {
-                            flag = 1;
-                            break;
-                        }
-
-                        visit[i + 1][j] = 1;
-                        visit[i + k + 1][j] = 1;
+            if (1 == map[i][j] - map[i + 1][j]) {
+                for (int k = 0; k < L; k++) {
+                    if (visited[i + k + 1][j] || (i + k + 1 >= N) || (map[i + k + 1][j] != map[i + 1][j])) {
+                        flag = 1;
+                        break;
                     }
-                }
-                else if (-1 == map[i][j] - map[i + 1][j]) {
 
-                    for (int k = 1; k < L; k++) {
-                        if (visit[i - k][j] || (i - k < 0) || (map[i - k][j] != map[i][j])) {
-                            flag = 1;
-                            break;
-                        }
-
-                        visit[i][j] = 1;
-                        visit[i - k][j] = 1;
-                    }
-                }
-
-                if (flag == 1) {
-                    for (int k = 0; k < N; k++) visit[k][j] = 0;
-                    break;
+                    visited[i + 1][j] = 1;
+                    visited[i + k + 1][j] = 1;
                 }
             }
-            else {
-                for (int k = 0; k < N; k++) visit[k][j] = 0;
+            else if (-1 == map[i][j] - map[i + 1][j]) {
+                for (int k = 0; k < L; k++) {
+                    if (visited[i - k][j] || (i - k < 0) || (map[i - k][j] != map[i][j])) {
+                        flag = 1;
+                        break;
+                    }
+
+                    visited[i][j] = 1;
+                    visited[i - k][j] = 1;
+                }
+            }
+            else if (map[i][j] != map[i + 1][j]) {
+                flag = 1;
                 break;
             }
+
+            if (flag) break;
         }
 
-        if (flag == 0) {
-            cout << j << endl;
-            ans++;
-        }
+        for (int k = 0; k < N; k++) visited[k][j] = 0;
+
+        if (!flag) ans++;
     }
 }
 
@@ -111,9 +105,10 @@ int main() {
         for (int j = 0; j < N; j++)
             cin >> map[i][j];
 
-    construct();
+    row();
+    column();
 
-    cout << ans;
+    cout << ans << endl;
 
     return 0;
 }
