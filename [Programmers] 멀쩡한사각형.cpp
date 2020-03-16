5712f2d35806a6d3e50e5e3b50acfacb12199e02
seoyeonKK <syeon0204@naver.com>
2020-03-16 23:47:42 +0900

    Create [Programmers] 멀쩡한사각형.cpp

---
+++ [Programmers] 멀쩡한사각형.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+
+long long solution(int w,int h)
+{
+  int gcd;
+	long long sum = (long long)w * (long long)h;
+	
+  if (w < h) {
+      for (int i = w; i > 0; i--) {
+          if ((w % i == 0) && (h % i == 0)) { 
+            gcd = i;
+            break;
+          }
+      } 
+  }
+  else {
+      for (int i = h; i > 0; i--) {
+          if ((w % i == 0) && (h % i == 0)) { 
+            gcd = i;
+            break;
+          }
+      }
+  }
+
+	return sum - gcd * ((w / gcd) + (h / gcd) - 1);
+}
