2704a6380722eb1e989301c987caae3ca722faa2
seoyeonKK <syeon0204@naver.com>
2020-03-27 00:13:12 +0900

    Update [Programmers] 예산.cpp

--- [Programmers] 예산.cpp
+++ [Programmers] 예산.cpp
@@ -5,53 +5,41 @@ using namespace std;
 
 int solution(vector<int> budgets, int M) {
     int answer = 0;
-    long sum = 0;
-    int old_mid;
-    int mid = 50000;
-    int max = 100000;
-    int min = 0;
-    
+
     sort(budgets.begin(), budgets.end());
     
-    // sum : 요청 금액의 합
-    for (int i = 0; i < budgets.size(); i++) {
-        sum += budgets[i];
-    }
-    
-    if (sum <= M)
-        return budgets[budgets.size() - 1];
-    
-    // mid : 상한액 후보
-    while (true) {
-        sum = 0;
-        
-        // sum : 상한가 고려한 요청 금액의 합
-        for (int i = 0; i < budgets.size(); i++) {
-            if (budgets[i] > mid) {
-                sum += mid;
-            }
-            else {
+    int max = budgets.back();
+    int min = 1;
+    int d = 1000000000;
+
+    while(max >= min){
+        int mid = (max + min)/2;
+
+        int sum = 0;
+
+        for(int i = 0; i < budgets.size(); i++){
+            if(budgets[i] <= mid){
                 sum += budgets[i];
             }
+            else{
+                sum += mid;
+            }
         }
-        
-        if (M < sum) {
-            max = mid;
+
+        if(sum == M){
+            answer = mid;
+            break;
         }
-        else if (M > sum) {
-            min = mid;
+        else if(sum > M) {
+            max = mid-1;
         }
-        else{
-            return mid;
-        }
-        
-        old_mid = mid;
-        mid = (min + max) / 2;
-        
-        if (mid == old_mid) {
-            return mid;
+        else {
+            if(M-sum < d){
+                answer = mid;
+                d = M-sum;
+            }
+            min = mid + 1;
         }
     }
-    
     return answer;
 }
