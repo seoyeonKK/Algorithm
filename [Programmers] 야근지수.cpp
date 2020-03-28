0b10375c9f4a06c0493a9b9089539191ae32d85b
seoyeonKK <syeon0204@naver.com>
2020-03-28 23:53:49 +0900

    정확도 O, 효율성 O
     매번 sort를 하지 않도록  priority queue를 사용하여 효율성이 높아졌습니다.

--- [Programmers] 야근지수.cpp
+++ [Programmers] 야근지수.cpp
@@ -1,23 +1,27 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-bool desc(int a, int b){ return a > b; }
-
 long long solution(int n, vector<int> works) {
     long long answer = 0;
+    priority_queue<int> q(works.begin(), works.end());
     
     while(n) {
-        sort(works.begin(), works.end(), desc);
-        if (works.front() == 0) break;
-        works.front()--;
+        int temp = q.top();
+        
+        if (temp == 0) break;
+        
+        q.pop();
+        q.push(temp-1);
         n--;
     }
     
-    for (int i = 0; i < works.size(); i++) {
-        answer += (works[i] * works[i]);
+    while(!q.empty()) {
+        answer += (q.top() * q.top());
+        q.pop();
     }
     
     return answer;
