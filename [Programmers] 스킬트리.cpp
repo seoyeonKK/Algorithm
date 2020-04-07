f68626021b0cab6668469d1f3e52da1651d1d7a3
seoyeonKK <syeon0204@naver.com>
2020-04-08 01:19:26 +0900

    Update [Programmers] 스킬트리.cpp

--- [Programmers] 스킬트리.cpp
+++ [Programmers] 스킬트리.cpp
@@ -1,34 +1,39 @@
 #include <string>
+#include <iostream>
 #include <vector>
 using namespace std;
 int solution(string skill, vector<string> skill_trees) {
     int answer = 0;
-    int tf = 1;
-    int check = 0;
+    bool flag = 1;
+    int nextIdx = 0;
+    
     for(int i = 0; i < skill_trees.size(); i++){
-        string vec_str = skill_trees[i];
-        for(int j = 0; j < vec_str.size(); j++){
-            // vec_str[j]와 find_num 비교하기
-            int find_num = skill.find(vec_str[j]);
-            // 범위에맞지않을 경우 
-            if(find_num < 0 || find_num > 30){
-                continue;
-            }
-            // 체크된 개수랑 find()한 개수가 틀릴경우 
-            if(find_num != check){
-                tf = 0;
+        string tree = skill_trees[i];
+        
+        for(int j = 0; j < tree.length(); j++){
+            int idx = skill.find(tree[j]);
+             
+            // skill에 없는 문자일 경우 넘어가기
+            if(idx < 0) continue;
+            
+            // idx가 nextIdx와 다를 경우 
+            if(idx != nextIdx){
+                flag = 0;
                 break;
             }
-            //동일한 문자가 있을경우 통과하면
+            // idx가 nextIdx와 같다면 nextIdx+1
             else{
-                check++;
+                nextIdx++;
             }
         }
-        if(tf){
+        
+        // 스킬이 순서대로 잘 있다면
+        if(flag){
             answer++;
         }
-        check = 0;
-        tf = 1;
+        
+        nextIdx = 0;
+        flag = 1;
     }
     return answer;
 }
