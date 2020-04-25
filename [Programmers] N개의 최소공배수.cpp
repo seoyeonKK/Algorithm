587a3f72ac33ec4d5c825855f2fbf6c6aaed13c9
seoyeonKK <syeon0204@naver.com>
2020-04-26 00:14:17 +0900

    Update [Programmers] N개의 최소공배수.cpp
    최소공배수(a, b) = a * b / 최대공약수(a, b)

--- [Programmers] N개의 최소공배수.cpp
+++ [Programmers] N개의 최소공배수.cpp
@@ -1,29 +1,26 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
+int GCD(int a, int b){
+    int c;
+    while(b != 0){
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+int LCM(int a, int b){
+    return a * b / GCD(a, b);
+}
 int solution(vector<int> arr) {
-    int answer = 1;
-    int div = 1;
-    
-    sort(arr.begin(), arr.end());
-    
-    for (int i = 0; i < arr.size(); i++) {
-        answer *= arr[i];
+    int answer = 0;
+    int lcm = 1;
+    for(int i = 0; i < arr.size(); i++){
+        lcm = LCM(lcm, arr[i]);
     }
-    
-    for (int i = 0; i < arr.size()-1; i++) {
-        for (int j = i+1; j < arr.size(); j++) { 
-            if (arr[j]%arr[i] == 0) {
-                div *= arr[i];
-            }
-        }
-    }
-    
-    answer /= div;
-    
+    answer = lcm;
     return answer;
 }
