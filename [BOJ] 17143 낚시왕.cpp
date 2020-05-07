85a4e9bebb7e2f3af0d69e37f868871b27527000
seoyeonKK <syeon0204@naver.com>
2020-05-07 23:46:34 +0900

    Update [BOJ] 17143 낚시왕.cpp
    [약 4시간 소요]
    
    - 끝까지 혼자서 붙들고 풀었다.
    
    - 어려운 시뮬레이션 문제이다.
    
    - 상어를 삭제할 때 편하기 위해 `sharkMap` struct에 `idx 변수`를 선언하였는데, 
    
      `size`라는 identity값이 있으니 idx는 없어도 무방할 것 같다. 
    
    
    
    **<낚시 시뮬레이션 과정>**
    
    (1) `visited`를 사용하여 이미 상어가 있는 지 확인하고
    
    (2) 있으면 크기가 작은 상어를 제거한다.
    
    (3) 이동을 완료 한 상어 `copyMap`을 본래의 `Map`으로 복사한다.
    
    (4) 어부가 있는 `column`에서 가장 가까운 `row`에 있는 상어를 잡는다.  
    
    
    
    **<상어 이동 과정>**
    
    (1) `vertical`, `horizontal` move를 나눠서 규칙에 맞게 구현하였다.
    
    (2) R, C 좌표가 범위 내에 있을 때까지, `while문`을 순회하며 범위 밖으로  `-/+` 되는 값을 더하고 빼준다.

--- [BOJ] 17143 낚시왕.cpp
+++ [BOJ] 17143 낚시왕.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#define MAX 100
+#define MAX 101
 using namespace std;
 
 
@@ -19,11 +19,20 @@ struct SharkMap {
 };
 
 int R, C, M;
-SharkMap map[MAX][MAX];
-vector<Shark> shark;
+int answer = 0;
 
-int copySharkMap () {
-    map[MAX][MAX] = {-1, -1, -1, -1 };
+vector<Shark> shark;
+SharkMap map[MAX][MAX] = {};
+SharkMap copyMap[MAX][MAX] = {};
+bool visited[MAX][MAX] = {false, };
+
+void copySharkMap () {
+
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            copyMap[i][j] = { 0, 0, 0, 0 };
+        }
+    }
 
     for (int i = 0; i < shark.size(); i++) {
         int r = shark[i].r;
@@ -32,16 +41,109 @@ int copySharkMap () {
         int d = shark[i].dir;
         int z = shark[i].size;
 
-        if (map[r][c].idx != -1) {
-            if (z > map[r][c].size) {
-                shark.erase(shark.begin() + map[r][c].idx );
+        if (visited[r][c]) {
+            if (z > copyMap[r][c].size) {
+                for (int j = 0; j < shark.size(); j++) {
+                    if (copyMap[r][c].size == shark[j].size) {
+                        shark.erase(shark.begin() + j);
+                    }
+                }
+                i--;
             }
             else {
-                shark.erase(shark.begin() + i );
+                for (int j = 0; j < shark.size(); j++) {
+                    if (z == shark[j].size) {
+                        shark.erase(shark.begin() + j);
+                    }
+                }
+                i--;
                 continue;
             }
         }
-        map[r][c] = { i, s, d, z };
+        copyMap[r][c] = { i, s, d, z };
+        visited[r][c] = true;
+    }
+
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            map[i][j].idx = copyMap[i][j].idx;
+            map[i][j].size = copyMap[i][j].size;
+            map[i][j].dir = copyMap[i][j].dir;
+            map[i][j].speed = copyMap[i][j].speed;
+            visited[i][j] = false;
+        }
+    }
+}
+
+int verticalMove(int y, int &dir) {
+    while (y < 1 || y > R) {
+        if (y < 1) {
+            y = 2 - y;
+            dir = 2;
+        }
+        if (y > R) {
+            y = R - (y - R);
+            dir = 1;
+        }
+    }
+    return y;
+}
+
+int horizontalMove(int x, int &dir) {
+    while (x < 1 || x > C) {
+        if (x < 1) {
+            x = 2 - x;
+            dir = 3;
+        }
+        if (x > C) {
+            x = C - (x - C);
+            dir = 4;
+        }
+    }
+    return x;
+}
+
+void fishing(int idx) {
+    for (int i = 1; i <= R; i++) {
+        if (map[i][idx].dir > 0) {
+            answer += map[i][idx].size;
+            for (int j = 0; j < shark.size(); j++) {
+                if (map[i][idx].size == shark[j].size) {
+                    shark.erase(shark.begin() + j);
+                }
+            }
+            map[i][idx] = { 0, 0, 0, 0 };
+            break;
+        }
+    }
+}
+
+void move () {
+    for (int i = 2; i <= C; i++) {
+        for (int j = 0; j < shark.size(); j++) {
+            if (shark[j].dir == 1) {
+                int y;
+                y = shark[j].r - shark[j].speed;
+                shark[j].r = verticalMove(y, shark[j].dir);
+            }
+            else if (shark[j].dir == 2) {
+                int y;
+                y = shark[j].r + shark[j].speed;
+                shark[j].r = verticalMove(y, shark[j].dir);
+            }
+            else if (shark[j].dir == 3) {
+                int x;
+                x = shark[j].c + shark[j].speed;
+                shark[j].c = horizontalMove(x, shark[j].dir);
+            }
+            else if (shark[j].dir == 4) {
+                int x;
+                x = shark[j].c - shark[j].speed;
+                shark[j].c = horizontalMove(x, shark[j].dir);
+            }
+        }
+        copySharkMap();
+        fishing(i);
     }
 }
 
@@ -51,11 +153,23 @@ int main () {
 
     cin >> R >> C >> M;
 
+    if (M == 0) {
+        cout << "0";
+        return 0;
+    }
+
     for (int i = 0; i < M; i++) {
         int r, c, s, d, z;
-        cin >> r >> c >>  s >> d >> z;
+        cin >> r >> c >> s >> d >> z;
 
         shark.push_back({ r, c, s, d, z });
     }
-}
 
+    copySharkMap();
+    fishing(1);
+    move();
+
+    cout << answer;
+
+    return 0;
+}
