baff3388213a57efea8e4d8bfda48afff3234b65
seoyeonKK <syeon0204@naver.com>
2020-05-14 23:20:55 +0900

    Create [Programmers] 최솟값 최댓값.cpp

---
+++ [Programmers] 최솟값 최댓값.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    vector<int> v;
+
+	stringstream ss(s);
+	string str;
+    
+	while(ss>>str) {
+		v.push_back(stoi(str));
+	}
+
+
+    sort(v.begin(), v.end());
+    
+    answer += to_string(v[0]);
+    answer += " ";
+    answer += to_string(v[v.size()-1]);
+    
+    return answer;
+}
