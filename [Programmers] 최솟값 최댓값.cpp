5db17a33619d2716caed73e1c743f105fbf849c2
seoyeonKK <syeon0204@naver.com>
2020-05-15 23:43:22 +0900

    StringStream 주석 추가

--- [Programmers] 최솟값 최댓값.cpp
+++ [Programmers] 최솟값 최댓값.cpp
@@ -10,14 +10,13 @@ string solution(string s) {
     string answer = "";
     vector<int> v;
 
+        // 띄어쓰기 단위 split
 	stringstream ss(s);
 	string str;
     
 	while(ss>>str) {
 		v.push_back(stoi(str));
 	}
-
-
     sort(v.begin(), v.end());
     
     answer += to_string(v[0]);
