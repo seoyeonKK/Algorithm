b093cea71bd916f6b5458892f4bb4e8662140220
seoyeonKK <syeon0204@naver.com>
2020-08-26 03:42:37 +0900

    Update [BOJ] 1260 bfs와 dfs.cpp

--- [BOJ] 1260 bfs와 dfs.cpp
+++ [BOJ] 1260 bfs와 dfs.cpp
@@ -1,61 +1,67 @@
 #include <iostream>
-#include <queue>
 #include <cstring>
+#include <queue>
+#define MAX 1001
+
 using namespace std;
 
-const int MAX = 1000;
-int N, M, V;
-int adjacent[MAX][MAX];
-bool visited[MAX];
+int map[MAX][MAX];
+int visited[MAX]; //정점의 개수만큼 visited
 queue<int> q;
+int n,m,v;
 
-void DFS(int idx) {
-	cout << idx << " ";
-	for (int i = 1; i < N; i++) {
-		if (adjacent[idx][i] && !visited[i]) {
-			visited[i] = 1;
-			DFS(i);
-		}
-	}
+void init(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 }
 
-void BFS(int idx) {
-	
-	q.push(idx);
-	visited[idx] = 1;
-
-	while (!q.empty()) {
-		idx = q.front();
-		q.pop();
-
-		cout << idx << " ";
-
-		for (int i = 1; i <= N; i++) {
-			if (adjacent[idx][i] && !visited[i]) {
-				visited[i] = 1;
-				q.push(i);
-			}
-		}
-	}
+void dfs(int v){
+    cout << v << " ";
+    visited[v] = 1;
+    for(int i = 1; i <= n; i++){
+        if(map[v][i] && !visited[i]){
+            dfs(i);
+        }
+    }
 }
 
-int main(void) {
-	cin >> M >> N >> V;
+void bfs(int v){
+    visited[v] = 1;
+    q.push(v);
+    
+    while(!q.empty()){
+        v = q.front();
+        q.pop();
+        
+        cout << v << " "; //bfs는 pop으로 방문함!! 
+        for(int i = 1; i <= n; i++){
+            if(map[v][i] && !visited[i]){
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
 
-	for (int i = 0; i <= M; i++) {
-		int from, to;
-		cin >> from >> to;
-		adjacent[from][to] = 1;
-		adjacent[to][from] = 1;
-	}
-
-	visited[V] = 1;
-	DFS(V);
-	cout << endl;
-	
-	memset(visited, false, sizeof(visited));
-	BFS(V);
-	cout << endl;
-
-	return 0;
+int main(void){
+    init();
+    cin >> n >> m >> v;
+    memset(map,0,sizeof(map));
+    memset(visited,0,sizeof(visited));
+    
+    for(int i = 0; i < m; i++){
+        int a, b; //간선을 연결, 연결되어있음을 1로 나타냄
+        cin >> a >> b;
+        map[a][b] = 1;
+        map[b][a] = 1;
+    }
+    
+    dfs(v);
+    cout << "\n";
+    //////////////
+    
+    memset(visited,0,sizeof(visited));
+    bfs(v);
+    cout << "\n";
+    return 0;
 }
