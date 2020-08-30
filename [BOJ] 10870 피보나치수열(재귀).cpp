25eb68dc26e6197f368055f921e6cdc488b238be
seoyeonKK <syeon0204@naver.com>
2020-08-31 01:20:11 +0900

    Create [BOJ] 10870 피보나치수열(재귀).cpp

---
+++ [BOJ] 10870 피보나치수열(재귀).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    
+    return fibo(n-2) + fibo(n-1);   
+}
+
+int main() {
+    int N;
+    cin >> N;
+    
+    cout << fibo(N);
+    
+    return 0;
+}
