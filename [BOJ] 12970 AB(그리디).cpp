1cdf376eca10187626d393c1d3d315e8d4b17c2e
seoyeonKK <syeon0204@naver.com>
2021-01-23 00:01:54 +0900

    Update [BOJ] 12970 AB(그리디).cpp
    주석 추가

--- [BOJ] 12970 AB(그리디).cpp
+++ [BOJ] 12970 AB(그리디).cpp
@@ -6,12 +6,13 @@ using namespace std;
 int main () {
     int N, K;
     cin >> N >> K;
-    for (int a = 0; a <= N; a++) {
-        int b = N-a;
-        if (a*b < K) continue; // 0 <= K <= a*b
-        vector<int> cnt(b+1);
+    
+    for (int a = 0; a <= N; a++) { // a의 갯수
+        int b = N-a; // b의 갯수
+        if (a*b < K) continue; // 0 <= K <= a*b 이다. 앞(aaa)뒤(bb) 최소 쌍.
+        vector<int> cnt(b+1); // b사이에 끼게 될 a의 위치
         for (int i = 0; i < a; i++) {
-            int x = min(b, K);
+            int x = min(b, K); // a가 추가될 수 있는 위치의 범위는 (K ~ b)
             cnt[x] += 1; // A가 i번 위치에 몇 개인지.
             K -= x;
         }
@@ -25,6 +26,8 @@ int main () {
         }
         cout << endl;
     }
+    
     cout << -1 << endl;
+    
     return 0;
 }
