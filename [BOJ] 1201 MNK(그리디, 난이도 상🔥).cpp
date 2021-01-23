7f6d9b794f9469ad65140b8bbcf4cefe4e044b29
seoyeonKK <syeon0204@naver.com>
2021-01-24 00:06:05 +0900

    Update [BOJ] 1201 MNK(그리디, 난이도 상🔥).cpp

--- [BOJ] 1201 MNK(그리디, 난이도 상🔥).cpp
+++ [BOJ] 1201 MNK(그리디, 난이도 상🔥).cpp
@@ -16,23 +16,30 @@ int main () {
     int N, M, K;
     cin >> N >> M >> K;
     vector<int> a(N);
+
+    // 최소 범위 : M+K-1(최소 1개를 공유할 수 있기 때문)
+    // 최대 범위 : MK+1(M+1인 수열이나 K+1인 수열을 반드시 만들기 때문 - 비둘기집 원리 증명)
     if ((M+K-1 <= N) && (N <= M*K)) {
         for (int i = 0; i < N; i++) {
-            a[i] = i+1;
+            a[i] = i+1; // 전체 증가 순열
         }
         vector<int> g;
         g.push_back(0);
-        g.push_back(K); // 그룹의 경계 0 ~ K
+        g.push_back(K); // 첫번째 그룹의 경계 0 ~ K 만들기 
         N -= K; // 남은 수열 갯수
         M -= 1; // 그룹 만들면 그룹 갯수 빼기
         int gs = M == 0 ? 1 : N/M;
         int r = M == 0 ? 0 : N%M; // N/M의 나머지
+
+        // 나머지 그룹의 경계 만들기
         for (int i = 0; i < M; i++) {
-            g.push_back(g.back()+gs + (r > 0 ? 1 : 0));
+            g.push_back(g.back()+gs + (r > 0 ? 1 : 0)); // 나머지 r이 있으면 1을 더함
             if (r > 0) {
                 r -= 1;
             }
         }
+        
+        // 각 그룹 뒤집기 
         for (int i = 0; i < g.size()-1; i++) {
             reverse(a.begin()+g[i], a.begin()+g[i+1]);
         }
