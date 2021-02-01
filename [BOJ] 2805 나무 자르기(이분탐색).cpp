6ac0b7bc86d78deb5e036b87f413891ffc2ac4d3
seoyeonKK <syeon0204@naver.com>
2021-02-01 18:41:53 +0900

    Update [BOJ] 2805 나무 자르기(이분탐색).cpp
    복습 및 주석 추가

--- [BOJ] 2805 나무 자르기(이분탐색).cpp
+++ [BOJ] 2805 나무 자르기(이분탐색).cpp
@@ -1,40 +1,48 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool check(vector<long long> &a, long long mid, long long m) {
-    int n = a.size();
+
+// mid길이의 절단기로 나무를 잘랐을 떄 m을 만족하는지 확인하는 함
+bool check(vector<long long> &tree, long long mid, long long m) { // check(나무벡터, 절단기 높이, 최소 m)
+    int n = tree.size();
     long long cnt = 0;
-    for (int i=0; i<n; i++) {
-        if (a[i]-mid > 0) {
-            cnt += a[i]-mid;
+    for (int i = 0; i < n; i++) {
+        if (tree[i]-mid > 0) {
+            cnt += (tree[i]-mid);
         }
     }
     return cnt >= m;
 }
+
 int main() {
     int n;
     long long m;
-    scanf("%d %lld",&n,&m);
-    vector<long long>a(n);
-    long long l=0;
-    long long r=0;
-    for (int i=0; i<n; i++) {
-        scanf("%lld",&a[i]);
-        if (r < a[i]) {
-            r = a[i];
+    cin >> n >> m;
+    
+    vector<long long> tree(n);
+    long long l = 0; // 절단기 길이의 범위(이분탐색) : 0 < X < 가장 높은 나무의 높이
+    long long r = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> tree[i];
+        if (r < tree[i]) { // 가장 높은 나무의 길이를 최댓값으로 설정
+            r = tree[i];
         }
     }
+
     long long ans = 0;
-    while (l <= r) {
+    while(l <= r) {
         long long mid = (l+r)/2;
-        if (check(a,mid,m)) {
-            ans = max(ans,mid);
-            l = mid+1;
-        } else {
+        if (check(tree, mid, m)) {
+             ans = max(ans, mid);
+             l = mid+1;
+        }
+        else {
             r = mid-1;
         }
     }
-    printf("%lld\n",ans);
+
+    cout << ans << endl;
+
     return 0;
 }
