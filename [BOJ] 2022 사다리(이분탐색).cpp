1b88a09187bbd470b340398bd07103409b56a41a
seoyeonKK <syeon0204@naver.com>
2021-02-06 02:04:06 +0900

    Update [BOJ] 2022 사다리(이분탐색).cpp

--- [BOJ] 2022 사다리(이분탐색).cpp
+++ [BOJ] 2022 사다리(이분탐색).cpp
@@ -1,25 +1,31 @@
-#include <cstdio>
-#include <cmath>
+#include <iostream>
 #include <algorithm>
+#include <math.h>
 using namespace std;
+
 int main() {
     double x, y, c;
-    while (scanf("%lf %lf %lf",&x,&y,&c) == 3) {
-        double left = 0;
-        double right = min(x, y);
-        while (abs(right-left) > 1e-6) {
-            double mid = (left + right) / 2.0;
-            double d = mid;
-            double h1 = sqrt(x*x-d*d);
-            double h2 = sqrt(y*y-d*d);
-            double h = (h1*h2)/(h1+h2);
-            if (h > c) {
-                left = mid;
-            } else {
-                right = mid;
-            }
+    cin >> x >> y >> c;
+
+    double left = 0;
+    double right = min(x, y);
+
+    for (int k = 0; k < 10000; k++) {
+        double mid = (left + right) / 2.0;
+        double d = mid;
+        double h1 = sqrt(x * x - d * d);
+        double h2 = sqrt(y * y - d * d);
+        double h = (h1 * h2) / (h1 + h2);
+
+        if (h > c) {
+            left = mid;
         }
-        printf("%.3lf\n",left);
+        else {
+            right = mid;
+        }
     }
-    return 0;
+
+    printf("%.3lf\n", left);
+
+return 0;
 }
