d3e4ac76d2c6fb51df07e57277ba9de3956ba87d
seoyeonKK <syeon0204@naver.com>
2021-04-19 17:38:47 +0900

    주석 추가 : [Programmers] 가장 먼 노드(BFS, 복습).cpp

--- [Programmers] 가장 먼 노드(BFS, 복습).cpp
+++ [Programmers] 가장 먼 노드(BFS, 복습).cpp
@@ -13,19 +13,23 @@ int solution(int n, vector<vector<int>> vertex) {
     int max = 0;
     int answer = 0;
     
+    // 간선 표시
     for(int i = 0; i < vertex.size(); i++) {
         connect[vertex[i][0]][vertex[i][1]] = 1;
         connect[vertex[i][1]][vertex[i][0]] = 1;
     }
 
+    // 1번 노드 큐 삽입
     visit[1] = true;
     q.push(1);
-    dist[1] = 0;
-    
+    dist[1] = 0; // 1번 노드 거리 = 0
+
+    // bfs
     while(!q.empty()) {       
         int cur = q.front();
         q.pop();       
         
+        // 인접 행렬 탐색
         for(int i = 1; i <= n; i++) {
             if(connect[cur][i] == 1 && !visit[i]) {
                 q.push(i);
